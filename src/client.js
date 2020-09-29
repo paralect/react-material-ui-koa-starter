938ecbe68ed262679366b7882e5497378c3d4c76
Mike Gagarin <m.gagarin@paralect.com>
2020-09-29 13:40:13 +0300

    Fix for syncing server and client side styles

--- src/client.js
+++ src/client.js
@@ -1,14 +1,26 @@
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { hydrate } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-hydrate(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+import App from './App';
+
+function Main() {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+hydrate(<Main />, document.getElementById('root'));
 
 if (module.hot) {
   module.hot.accept();
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import express from 'express';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 import App from './App';
 
@@ -9,45 +10,62 @@ import App from './App';
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const server = express();
-server
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', (req, res) => {
-    const context = {};
-    const markup = renderToString(
+
+function renderFullPage(html, css) {
+  return `
+    <!doctype html>
+    <html lang="">
+    <head>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta charset="utf-8" />
+      <title>Title</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style id="jss-server-side">${css}</style>
+      ${
+        assets.client.css
+          ? `<link rel="stylesheet" href="${assets.client.css}">`
+          : ''
+      }
+      ${
+        process.env.NODE_ENV === 'production'
+          ? `<script src="${assets.client.js}" defer></script>`
+          : `<script src="${assets.client.js}" defer crossorigin></script>`
+      }
+    </head>
+    <body>
+      <div id="root">${html}</div>
+    </body>
+    </html>
+  `;
+}
+
+function handleRender(req, res) {
+  const context = {};
+  const sheets = new ServerStyleSheets();
+
+  // Render the component to a string.
+  const html = renderToString(
+    sheets.collect(
       <StaticRouter context={context} location={req.url}>
         <App />
       </StaticRouter>
-    );
+    )
+  );
 
-    if (context.url) {
-      res.redirect(context.url);
-    } else {
-      res.status(200).send(
-        `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Title</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        }
-        ${
-          process.env.NODE_ENV === 'production'
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`
-        }
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-    </body>
-</html>`
-      );
-    }
-  });
+  // Grab the CSS from the sheets.
+  const css = sheets.toString();
+
+  // Send the rendered page back to the client.
+  if (context.url) {
+    res.redirect(context.url);
+  } else {
+    res.status(200).send(renderFullPage(html, css));
+  }
+}
+
+server
+  .disable('x-powered-by')
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .get('/*', handleRender);
 
 export default server;
